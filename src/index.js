cc78ad42eb53e84982aa1db3dd196f438b9f4d87
pand4monium <229127319+pand4monium@users.noreply.github.com>
2025-09-30 15:09:35 +0800

    Added function to about-btn

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import { loadHome } from "./pages/home";
 import { loadMenu } from "./pages/menu";
 import { loadAbout } from "./pages/about";
 
-loadAbout();
+loadHome();
 
 document.getElementById("home-btn")
         .addEventListener("click", loadHome);
 
 document.getElementById("menu-btn")
         .addEventListener("click", loadMenu);
+
+document.getElementById("about-btn")
+        .addEventListener("click", loadAbout);
